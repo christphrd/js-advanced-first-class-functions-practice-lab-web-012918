8804d89087be02cf34324f709182a0a2ddf1ecc2
github username <github email address>
2018-03-12 15:15:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (driver){
+    console.log(driver[`name`]);
+  });
+};
+
+function logDriversByHometown(drivers, location) {
+  hometownMatch = drivers.filter(driver => driver[`hometown`] === location);
+  hometownMatch.forEach(function (driver){console.log(driver[`name`])});
+};
+
+function driversByRevenue(drivers) {
+  const driversCopy = drivers.slice()
+  sorted = driversCopy.sort(function (a,b){
+    return a["revenue"] - b["revenue"];
+  });
+  return sorted
+};
+
+function driversByName(drivers) {
+  const driversCopy = drivers.slice()
+  sorted = driversCopy.sort(function (a,b){
+    return a["name"].localeCompare(b["name"]);
+  });
+  return sorted
+}
+
+function totalRevenue(drivers) {
+  const reduceRevenue = function (agg, el, i, arr) {
+    return agg + el["revenue"]
+  };
+  return drivers.reduce(reduceRevenue,0);
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
